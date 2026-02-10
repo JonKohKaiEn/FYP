4ddbf971ba7e014fd6942305299a2161c1814ddb
JonKohKaiEn <jonkoh@Computer.>
2026-02-11 02:12:01 +0800

    Added question and model iteration testing, added NalaClaudeWrapper

--- nala_api_test.py
+++ nala_api_test.py
@@ -14,17 +14,22 @@ system_prompt: str = """
 You will be given a question. You are to extract the topics that are tested in the question.
 **Only** respond with a list of topics), separated by commas.
 """
-user_prompt: str = """
-Using the Cauchy-Riemann equations, determine the analycity of the function $f(z)=z^2-2z+3$ and find its derivative.
-"""
+
 # load question bank
-with open("question_bank.txt") as f:
+with open("question_bank.txt", mode="r") as f:
     question_bank = [line.rstrip() for line in f.readlines()]
 
+output: list = []
 for question in question_bank:
     print(f"Question: {question}")
+    output.append(f"Question: {question}\n")
     for llm_name, llm in llm_list:
         print(f"Current Model: {llm_name}")
         response: str = llm.invoke(system_prompt, question)
         print(f"Response: {response}")
+        output.append(f"Model: {llm_name} \t Response: {response}\n")
+    output.append("=" * 20 + "\n")
     print("=" * 20)
+
+with open("topic_output.txt", mode="w") as f:
+    f.writelines(output)
